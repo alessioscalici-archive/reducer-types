c1e3550af5bcf7bc770505e4b9cad21752b85ae5
Alessio Scalici <alessio.scalici@olx.com>
2018-11-30 13:26:58 +0100

    multiple mountpoints test

--- test/modules.spec.js
+++ test/modules.spec.js
@@ -14,21 +14,23 @@ const model = {
 };
 
 
-describe('must work with mountpoints', () => {
-  const MOUNT_POINT = 'myPlugin';
-  const { selectors, actions, reducer } = buildModule(model)([MOUNT_POINT]);
-  const { getArticlesById } = selectors;
+describe('must work with multiple mountpoints', () => {
+  const MOUNT_POINT_1 = 'myPlugin';
+  const MOUNT_POINT_2 = 'myAddOn';
+  const m1 = buildModule(model)([MOUNT_POINT_1]);
+  const m2 = buildModule(model)([MOUNT_POINT_2]);
   let reduxStore;
 
   beforeEach(() => {
     reduxStore = createStore(combineReducers({
-      [MOUNT_POINT]: reducer,
+      [MOUNT_POINT_1]: m1.reducer,
+      [MOUNT_POINT_2]: m2.reducer,
     }));
   });
 
   it('with the correct structure and initial state', () => {
     const rootState = reduxStore.getState();
-    const state = rootState[MOUNT_POINT];
+    const state = rootState[MOUNT_POINT_1];
 
     expect(typeof state.articles).toBe('object');
     expect(state.articles.byId).toEqual({});
@@ -37,15 +39,16 @@ describe('must work with mountpoints', () => {
   });
 
   it('that reacts to the actions', () => {
-    reduxStore.dispatch(actions.articles.byId.entry('myKey', 123));
+    reduxStore.dispatch(m1.actions.articles.byId.entry('myKey', 123));
+    reduxStore.dispatch(m2.actions.articles.byId.entry('myKey', 456));
     const rootState = reduxStore.getState();
-    const state = rootState[MOUNT_POINT];
-    expect(state.articles.byId).toEqual({ myKey: 123 });
+    expect(rootState[MOUNT_POINT_1].articles.byId).toEqual({ myKey: 123 });
+    expect(rootState[MOUNT_POINT_2].articles.byId).toEqual({ myKey: 456 });
   });
 
   it('that works with selectors', () => {
     const rootState = reduxStore.getState();
-    expect(getArticlesById(rootState))
-      .toEqual(rootState[MOUNT_POINT].articles.byId);
+    expect(m1.selectors.getArticlesById(rootState))
+      .toEqual(rootState[MOUNT_POINT_1].articles.byId);
   });
 });
